6f3c9f2a471a84803f509a480c35c021dfcd2eee
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-08-13 15:22:17 -0400

    Bug fix, update CORS config

--- backend/src/main/java/io/john/amiscaray/videosharingdemo/VideoSharingDemoApplication.java
+++ backend/src/main/java/io/john/amiscaray/videosharingdemo/VideoSharingDemoApplication.java
@@ -19,7 +19,7 @@ public class VideoSharingDemoApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
 
